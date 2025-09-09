5b4fdf8d397630f161ca378eb0591bdb73ec4c33
BOURRICAUD Antoine <a.bourricaud@rubika-edu.com>
2025-09-09 16:12:37 +0200

    Ajout des inputs de déplacements mobiles

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private CharacterController characterController;
+    private Vector2 moveInput;
+    private Vector3 velocity;
+    private float gravity = -9.8f;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        Vector3 move =  new Vector3(moveInput.x, 0, moveInput.y);
+        characterController.Move(move * speed * Time.deltaTime);
+        
+        velocity.y = gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
